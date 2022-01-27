e7a7689f6decebbf3597efa480caa6a8bad2d6a0
gustavoeob <gustavoeliasoropezabaez@gmail.com>
2022-01-26 22:29:44 -0300

    creating landing page

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 export const Product = ({ name, price, stock }) => {
   return(
-    <div>
-        <p>Product: { name }</p>
-        <p>Price: ${ price }</p>
-        <p>Stock: { stock }</p>
-        
+    <div className="Product">
+        <p className="item-list">Product: { name }</p>
+        <p className="item-list">Price: ${ price }</p>
+        <p className="item-list">Stock: { stock }</p>
     </div>
   ); 
 };
