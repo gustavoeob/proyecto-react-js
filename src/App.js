86bbbfc02f86f5f6ce012d7919e6f02509babf0b
Gustavo Oropeza <gustavoeliasoropezabaez@gmail.com>
2022-02-04 12:56:51 -0300

    git push test

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import ResponsiveAppBar from './Components/Navbar/Navbar';
 import ContainerProductList from './Components/container-product-list/ContainerProductList'
 import React, {useEffect} from 'react';
-
+ 
+let setCount;
 
 const App = () => {
   useEffect(() =>{
     const timer = setTimeout(() =>{
+      setCount('fvsc')
     }, 2000);
     return () => clearTimeout(timer);
   }, []);
