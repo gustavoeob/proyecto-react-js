a75b734cfe8e4b95c99495d7e806ff10d2fae714
Gustavo Oropeza <gustavoeliasoropezabaez@gmail.com>
2022-02-05 19:14:55 -0300

    ProductDetail.js and ContainerProductDetail.js  created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import ResponsiveAppBar from './Components/Navbar/Navbar';
 import ContainerProductList from './Components/container-product-list/ContainerProductList'
-import React, {useEffect} from 'react';
- 
-let setCount;
+
 
 const App = () => {
-  useEffect(() => {
-    const timer = setTimeout(() => console.log("Hello, World!"), 3000);
-    return () => clearTimeout(timer);
-  }, []);
+
 
   return ( 
     <div id="App">
