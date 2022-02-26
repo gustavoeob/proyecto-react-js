cf88cf8473af04bd7cd55862398930d32bea070d
gustavoeob <gustavoeliasoropezabaez@gmail.com>
2022-02-26 02:10:06 -0300

    cart action buttons fixed

--- src/Components/Navbar/Cart.js
+++ src/Components/Navbar/Cart.js
@@ -1,33 +1,61 @@
-import React, {useContext} from 'react'
-import { CartContext } from '../Context/CartContext'
+import React, { useContext } from "react";
+import { CartContext } from "../Context/CartContext";
+import remove from "../Navbar/Img/minus.png"
+import {Link} from 'react-router-dom'
 
 const Cart = () => {
-  const { cartData, clearCart, removeItem} = useContext(CartContext);
+  const { cartData, clearCart, removeItem } = useContext(CartContext);
 
   return (
-    <div>
-    
-    {cartData=== 0 ? <div className="cart-empty-msg">HOLA</div> :
-    cartData.map((item) =>(
-      <div key={item.id} className="cart-items">
-        <div className="cart-item-left">
-          <p className="cart-item cart-item-title">{item.name}</p>
-          <p className="cart-item cart-item-image">{item.image}</p>
-          <p className="cart-item cart-item-price">{item.price}</p>
+    <>
+      {!cartData.length ? (
+        <div className="empty-cart-container">
+          <p className="cart-empty-msg">Nothing here yet! 👹</p>
+          <Link to='/'>
+          <Link className="not-found-redirect" to='/'> Go home</Link>
+          </Link>
         </div>
-        <div className="cart-item-right">
-          <p className="cart-item cart-item-description">{item.description}</p>
-          <button  className="remove-cart-item"onClick={() => removeItem(item)}> X </button>
-        </div>
-      </div>
-
-    ))} 
-      <div className="cart-action-btn"> 
-            <button className="remove-all-cart-items" onClick={() => clearCart()}>Clear All</button> 
-      </div>
-  
-    </div>
+      ) 
+      : 
+      (
+        cartData.map((item) => (
+          <>
+          <>
+            <div>
+              <div key={item.id} className="cart-items">
+                <div className="cart-item-left">
+                  <p className="cart-item cart-item-title">{item.name}</p>
+                  <p className="cart-item cart-item-image">{item.image}</p>
+                  <p className="cart-item cart-item-price">{item.price}</p>
+                </div>
+                <div className="cart-item-right">
+                  <p className="cart-item cart-item-description">
+                    {item.description}
+                  </p>
+                  <button
+                    className="remove-cart-item"
+                    onClick={() => removeItem(item)}>
+                    <img className="remove-icon" src={remove}/>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <>
+            </>
+          </>
+              <div className="cart-action-btn">
+                <button
+                  className="remove-all-cart-items"
+                  onClick={() => clearCart()}>
+                  Clear All
+                </button>
+              </div>
+              </>
+        ))
+              
+      )} 
+    </>
   );
 };
 
-export default Cart
+export default Cart;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import App from './App';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBteQQpbu_oTO-iHxk8afjWB-xZEi3sa8E",
+  authDomain: "whoopie-co.firebaseapp.com",
+  projectId: "whoopie-co",
+  storageBucket: "whoopie-co.appspot.com",
+  messagingSenderId: "463487174442",
+  appId: "1:463487174442:web:8d92f9bfae564109978053",
+  measurementId: "G-2XK60DG0TM"
+};
+
+// const analytics = getAnalytics(app);
+
+
 ReactDOM.render(
   <React.StrictMode>
       <App />
